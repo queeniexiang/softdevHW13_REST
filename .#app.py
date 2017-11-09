fce3d91984def17971ad5bf4346d55d9ce5bd64a
queeniexiang <wxiang1@stuy.edu>
2017-11-09 12:10:23 -0500

    added

--- .#app.py
+++
@@ -1,1 +0,0 @@
-qx-macbookpro@Queenies-MacBook-Pro.local.49328
--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import urllib2
-from flask import Flask, session, url_for, redirect, render_template, request
+'''from flask import Flask, session, url_for, redirect, render_template, request
 import os
 
 #App instantiation
 app = Flask(__name__)
+'''
 
 def parse():
     uResp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=3pFWSza21V46Ci5KCCBmXDpixhLcHDCZFZ6F8xuT")
-    print(uResp)
+    site_content = uResp.read()
+    
+    d = json.loads(site_content)
+    print(d)
+    
 
 parse()
 
 
     
 
-if __name__ == "__main__":
+''' if __name__ == "__main__":
     app.debug = True
 
 app.run()
+''' 
 
 
     
