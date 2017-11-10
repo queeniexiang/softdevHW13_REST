4b67c158b511e2d6f58661f927be4ba3d152f1ed
queeniexiang <wxiang1@stuy.edu>
2017-11-09 23:20:37 -0500

    Finished

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 import urllib2
-import json 
-'''from flask import Flask, session, url_for, redirect, render_template, request
+import json
+from flask import Flask, session, url_for, redirect, render_template, request
 import os
 
 #App instantiation
 app = Flask(__name__)
-'''
 
+
+@app.route("/")
 def parse():
     uResp = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=3pFWSza21V46Ci5KCCBmXDpixhLcHDCZFZ6F8xuT")
     site_content = uResp.read()
     
     d = json.loads(site_content)
     explanation = d['explanation']
-    print(explanation)
+    img_src = d['hdurl']
+    date = d['date']
+
+    return render_template("display.html", explanation = explanation, img_src = img_src, date = date);
     
 
-parse()
-
-
-    
-
-''' if __name__ == "__main__":
+if __name__ == "__main__":
     app.debug = True
 
 app.run()
-''' 
+ 
 
 
     
